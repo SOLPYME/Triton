e901e1a15d16df0911e28685847eb3acda524d2a
Juan Manuel Pardo García <solpyme@outlook.com>
2022-11-16 00:33:15 +0100

    We add the classes of the Common Domain library, which belongs to the Kernel of the solution. The Model Validation Exceptions class has also been added, as well as the Behavior for Validation and Unhandled Exception

---
+++ src/Kernel/Core/Domain/Triton.Core.Domain.Common/BaseDomainModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Triton.Core.Domain.Common
+{
+    public class BaseDomainModel
+	{
+        public BaseDomainModel()
+        {
+            Id = new Guid();
+        }
+
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
+
