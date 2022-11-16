1779a299a508e19b63ad61c79eafb70f6cdd41a7
SOLPYME <104170677+SOLPYME@users.noreply.github.com>
2022-11-16 19:58:37 +0100

    Added DataAnnotations to fields CreatedBy and LastModifiedBy

--- src/Kernel/Core/Domain/Triton.Core.Domain.Common/BaseDomainModel.cs
+++ src/Kernel/Core/Domain/Triton.Core.Domain.Common/BaseDomainModel.cs
@@ -1,12 +1,16 @@
-﻿namespace Triton.Core.Domain.Common
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Triton.Core.Domain.Common
 {
     public class BaseDomainModel
 	{
         public int Id { get; set; }
         public DateTime? CreatedDate { get; set; }
+        [Column(TypeName = "VarChar(100)")]
         public string? CreatedBy { get; set; }
 
         public DateTime? LastModifiedDate { get; set; }
+        [Column(TypeName = "VarChar(100)")]
         public string? LastModifiedBy { get; set; }
     }
 }
